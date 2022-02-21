abcff243afac1adc765f2bd6f077cbcb2347a285
RuthTadesse <tib6297@gmail.com>
2022-02-21 11:56:28 -0500

    commit tree 2968213d00ea77f44d3ac73bdd8eaf9d3dc55321

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function scuberGreetingForFeet(distance) {
-  // Write your code here!
-
-  if (distance <= 400) {
-  return 'This one is on me!'
-  } else if(distance > 2000 && distance <= 2500) {
-  return 'I will gladly take your thirty bucks.'
-  } else if (distance > 2500) {
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!'
+  }
+  else if (distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
     return 'No can do.'
   }
 }
@@ -13,24 +13,18 @@ function scuberGreetingForFeet(distance) {
 
 
 function ternaryCheckCity(city){
-  // Write your code here!
-  
-  let message = city === 'NYC' ? "Ok, sounds good." : "No go.";
-  return message;
+  return city === 'NYC'?'Ok, sounds good.':'No go.'
 }
 
-
-
-function switchOnCharmFromTip(amount){
-  // Write your code here!
-
-  switch (amount) {
-    case 'generous':
-    return "Thank you so much.";
-    case 'not as generous':
-      return "Thank you.";
-      default: 
-      return "Bye.";
-  }
+function switchOnCharmFromTip(tip){
+ switch(tip){
+   case 'generous':
+     return 'Thank you so much.'
+     case 'not as generous':
+   return 'Thank you.'
+  default:
+    return 'Bye.'
+ }
+ 
 
 }
